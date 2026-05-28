c87a6b7885c36830e13f0cb21b28e3a344065106
Erick <erickfabian1309@gmail.com>
2026-05-28 16:54:02 -0500

    Montaje inicial

---
+++ script.js
@@ -0,0 +1,13 @@
+// script.js
+
+console.log("Feed95 website loaded!");
+
+/*
+  Aquí luego puedes agregar:
+  - animaciones
+  - menú responsive
+  - tema oscuro/claro
+  - scroll suave
+  - changelog dinámico
+  - conexión GitHub API
+*/
