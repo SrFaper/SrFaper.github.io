ca0670ef960b0687f5c2e66da71fbc0cbe7c72bb
Erick <erickfabian1309@gmail.com>
2026-05-28 17:06:32 -0500

    Esqueleto base

--- script.js
+++ script.js
@@ -1,13 +1,36 @@
-// script.js
-
 console.log("Feed95 website loaded!");
 
 /*
-  Aquí luego puedes agregar:
+  Ideas futuras:
+  - cambiar fondo dinámicamente
+  - consumir releases desde GitHub API
+  - mostrar versión automáticamente
   - animaciones
-  - menú responsive
-  - tema oscuro/claro
-  - scroll suave
   - changelog dinámico
-  - conexión GitHub API
+  - tema oscuro/claro
 */
+
+// Pequeña animación de entrada
+
+window.addEventListener("load", () => {
+
+  const hero = document.querySelector(".hero-content");
+
+  hero.animate(
+    [
+      {
+        opacity: 0,
+        transform: "translateY(20px)"
+      },
+      {
+        opacity: 1,
+        transform: "translateY(0)"
+      }
+    ],
+    {
+      duration: 800,
+      easing: "ease-out"
+    }
+  );
+
+});
